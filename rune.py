8e3d1a225f32ecf4855939082b891de1a1c4f7f5
lidapar <102316866+lidapar@users.noreply.github.com>
2022-03-26 21:50:50 +0700

    Add files via upload

---
+++ rune.py
@@ -0,0 +1,22 @@
+import subprocess
+import time
+
+subprocess.Popen(["python3","e1.py"])
+time.sleep(120)
+subprocess.Popen(["python3","e2.py"])
+time.sleep(120)
+subprocess.Popen(["python3","e3.py"])
+time.sleep(120)
+subprocess.Popen(["python3","e4.py"])
+time.sleep(120)
+subprocess.Popen(["python3","e5.py"])
+time.sleep(120)
+subprocess.Popen(["python3","e6.py"])
+time.sleep(120)
+subprocess.Popen(["python3","e7.py"])
+time.sleep(120)
+subprocess.Popen(["python3","e8.py"])
+time.sleep(120)
+subprocess.Popen(["python3","e9.py"])
+time.sleep(120)
+subprocess.Popen(["python3","e10.py"])
